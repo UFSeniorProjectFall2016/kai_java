4fb9ed249139f9f88c54be0b5cb50f7333e3da7c
Kinderley Charles <kinderleycharles@aol.com>
2016-11-16 20:44:02 -0500

    Local Database added. Device status are loaded from local db files. BootUp state support loading database. Sending state temporarily save device status. Error states added. Pinging mechanism added for system

--- kai/src/kai/system/system/Kai.java
+++ kai/src/kai/system/system/Kai.java
@@ -4,45 +4,61 @@ public class Kai {
 	// Class Members definition
 	private String webUri;
 	private String rosUri;
+	private String dbUri;
 	private int rosPort;
-	
+
 	RosConnection rosConn;
 	SocketConnection webConn;
-	
-	public Kai(String webUri, String rosUri, int rosPort) {
+	DatabaseConnection dbConn;
+
+	public Kai(String webUri, String rosUri, int rosPort, String dbUri) {
 		this.webUri = webUri;
 		this.rosUri = rosUri;
 		this.rosPort = rosPort;
+		this.dbUri = dbUri;
 	}
-	
+
 	public void connect() {
 		this.connectWeb();
 		this.connectRos();
 	}
-	
+
 	public void connectWeb() {
-		if(!this.webConnected()) {
-			webConn = new SocketConnection(webUri);
+		// Create a web connection
+		// Wait for connection to set or 5ms
+		webConn = new SocketConnection(webUri);
+		long strt = System.currentTimeMillis();
+		while ( !this.webConnected() && (System.currentTimeMillis()-strt < 5000) ) {}
+	}
+
+	public void connectRos() {
+		if (!this.rosConnected()) {
+			rosConn = new RosConnection(rosUri, rosPort);
 		}
 	}
-	
-	public void connectRos() {
-		rosConn = new RosConnection(rosUri, rosPort);
+
+	public void connectDB() {
+		dbConn = new DatabaseConnection(dbUri);
+		this.dbUri = dbConn.getDBUri();
 	}
-	
+
 	public RosConnection getROSConn() {
 		return rosConn;
 	}
-	
+
 	public SocketConnection getWebConn() {
 		return webConn;
 	}
-	
-	public boolean webConnected() {
-		return (webConn==null) ? false : webConn.isConnected();
+
+	public DatabaseConnection getDBConn() {
+		return dbConn;
 	}
-	
+
+	public boolean webConnected() {
+		return (webConn == null) ? false : webConn.isConnected();
+	}
+
 	public boolean rosConnected() {
-		return true;
+		return (rosConn == null) ? false : rosConn.isConnected();
 	}
 }
